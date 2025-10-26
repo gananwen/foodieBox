aaa6a579beba9715293950c9588d4923bc5b0cac
elly-02 <elysha6a@gmail.com>
2025-10-27 00:54:20 +0800

    Save current code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.foodiebox"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdkVersion(flutter.minSdkVersion.toInt())
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
